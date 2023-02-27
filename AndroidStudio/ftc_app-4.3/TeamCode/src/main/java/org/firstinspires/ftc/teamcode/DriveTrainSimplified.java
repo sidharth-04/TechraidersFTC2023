9165ff5eb89278cb38b48e72667cc71c1d50ed91
Sidharth Roy <68526404+sidharth-04@users.noreply.github.com>
2023-02-27 16:18:56 +0530

    Android Config

---
+++ AndroidStudio/ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainSimplified.java
@@ -0,0 +1,122 @@
+package ftcAdventure;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+@TeleOp
+public class DriveTrainSimplified extends LinearOpMode {
+    private DcMotorEx frontLeft;
+    private DcMotorEx frontRight;
+    private DcMotorEx backRight;
+    private DcMotorEx backLeft;
+    public BNO055IMU imu;
+
+    Orientation angles;
+    Acceleration gravity;
+
+    @Override
+    public void runOpMode() {
+        // Drivercentric Variables
+        double x;
+        double y;
+        double rx;
+        double robotAngle;
+
+        // Drivetrain Motors
+        frontLeft = hardwareMap.get(DcMotorEx.class, "B0");
+        frontRight = hardwareMap.get(DcMotorEx.class,"A0");
+        backRight = hardwareMap.get(DcMotorEx.class,"A1");
+        backLeft = hardwareMap.get(DcMotorEx.class,"B1");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Drivercentric Initialization
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        composeTelemetryMadarchod();
+
+        // Wait for player to press start
+        waitForStart();
+
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        while (opModeIsActive()) {
+            // ----------------------------------
+            // DRIVETRAIN
+            // ----------------------------------
+            // Get the coordinates of the joystick in a x-y axis
+            x = gamepad1.left_stick_x;
+            y = -gamepad1.left_stick_y;
+            rx = gamepad2.right_stick_x;
+
+            // Angle from the imu (internal angle of the robot)
+            robotAngle = getAngle();
+
+            // Calculate the rotated x and y
+            double rotX = x * Math.cos(-robotAngle) - y * Math.sin(-robotAngle);
+            double rotY = x * Math.sin(-robotAngle) + y * Math.cos(-robotAngle);
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY*Math.abs(rotY) + rotX*Math.abs(rotX) + rx);
+            double backLeftPower = (rotY*Math.abs(rotY) - rotX*Math.abs(rotX) + rx);
+            double frontRightPower = (rotY*Math.abs(rotY)  - rotX*Math.abs(rotX) - rx);
+            double backRightPower = (rotY*Math.abs(rotY)  + rotX*Math.abs(rotX) - rx);
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+            telemetry.addData("current backleft", backLeft.getPower());
+            telemetry.addData("current frontleft", frontLeft.getPower());
+            telemetry.addData("current backright", backRight.getPower());
+            telemetry.addData("current frontright", frontRight.getPower());
+            // telemetry.update();
+        }
+    }
+
+    // Utility Functions
+    void composeTelemetryMadarchod() {
+        telemetry.addAction(new Runnable() {
+            @Override
+            public void run() {
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+            }
+        });
+    }
+
+    public double getAngle() {
+        return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle * Math.PI / 180;
+    }
+}
